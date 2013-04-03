f60b2bba148c3123196221d39832d9dc9a086b5d
Mason Silber <mds2161@columbia.edu>
2013-04-03 00:24:13 -0400

    Layout of GUI now much more stable, simpler. Console is there as well (though print is not implemented yet)

--- RacecarGUI/racecar.py
+++ RacecarGUI/racecar.py
@@ -80,8 +80,8 @@ class CarDirection:
 
 class Car:
     def __init__(self):
-        self.x = 0
-        self.y = 0
+        self.position_x = 0
+        self.position_y = 0
         self.wheel_direction = WheelDirection.STRAIGHT
         #Car direction starts facing right
         self.car_direction = CarDirection()
@@ -93,24 +93,19 @@ class Car:
     #UI animation will need to be done moving x and y simultaneously
     def drive(self, steps):
         if self.wheel_direction == WheelDirection.STRAIGHT:
-            self.x += self.car_direction.x * steps
-            self.y += self.car_direction.y * steps
+            self.position_x += self.car_direction.x * steps
+            self.position_y += self.car_direction.y * steps
         elif self.wheel_direction == WheelDirection.RIGHT:
             for _ in range(steps):
-                self.turn_right()
+                self.car_direction.turn_right()
         else:
             for _ in range(steps):
-                self.turn_left()
+                self.car_direction.turn_left()
     
-    def turn_right(self):
-        self.car_direction.turn_right()
-
-    def turn_left(self):
-        self.car_direction.turn_left()
-        
     #Decided on a 10:1 pixels to steps ratio
-    def steps_to_pixels(self, steps):
-        return 10*steps
+
+def steps_to_pixels(self, steps):
+	return 10*steps
 
 #UI methods
 def generate_program(code):
@@ -123,28 +118,44 @@ def generate_program(code):
 def translate_car(steps):
     global car
 		
-
-'''
     #Just an example for now
     #Move to the left
-    for _ in range(0,10*int(steps)):
+    for _ in range(0,steps_to_pixels(int(steps))):
         time.sleep(0.025)
-        canvas.move(car.car_object,1,0)
+        canvas.move(car.car_object,car.car_direction.x,car.car_direction.y)
         canvas.update()
+    '''
     #Rotate clockwise
     for i in range(0,45):
         time.sleep(0.025)
         canvas.delete(car.car_object)
         car.image_tk = ImageTk.PhotoImage(car.image.rotate(i+1))
         car.car_object = canvas.create_image(30+10*int(steps),250, image=car.image_tk)
+        car.car_direction.y = -1
         canvas.update()
     #Move down
     for _ in range(0,10*int(steps)):
         time.sleep(0.025)
-        canvas.move(car.car_object,1,-1)
+        canvas.move(car.car_object,car.car_direction.x,car.car_direction.y)
         canvas.update()
 '''
 
+def rotate_car(steps, direction):
+		global car
+		
+		for i in range(0,int(steps)):
+				time.sleep(0.025)
+				canvas.delete(car.car_object)
+				
+				if direction == WheelDirection.left:
+						car.image_tk = ImageTk.PhotoImage(car.image.rotate(i+1))
+				elif direction == WheelDirection.right:
+						car.image_tk = ImageTk.PhotoImage(car.image.rotate(i+1))
+				else:
+						return
+				
+				car.car_object = canvas.create_image(car.position_x, car.position_y, image=car.image_tk)
+				canvas.update()
 
 def open_file():
     global current_program
@@ -215,50 +226,64 @@ def clear():
                             "Are you sure you want to delete all of your code?"):
         code.delete(1.0,END)
 
+#car object
+car = Car()
+
 #User interface
 root = Tk()
 root.title('Racecar')
-root.rowconfigure('all',minsize=100)
-root.columnconfigure('all',minsize=100)
 window_width = root.winfo_screenwidth()
 window_height = root.winfo_screenheight()
-root.geometry    ("%dx%d"%(window_width,window_height))
+root.geometry    ("%dx%d"%(window_width-100,window_height-100))
 
 menu_bar = Menu(root)
 menu = Menu(menu_bar, tearoff=0)
-menu.add_command(label="Open", command = lambda: open_file())
-menu.add_command(label="Save", command = lambda: save())
+menu.add_command(label="Open", command = open_file)
+menu.add_command(label="Save", command = save)
 menu.add_separator()
-menu.add_command(label="Quit", command = lambda: exit())
+menu.add_command(label="Quit", command = exit)
 menu_bar.add_cascade(label="File",menu=menu)
 
 root.config(menu=menu_bar)
 
+#frame for left side of window
+left_frame = Frame(root)
+
+#label for code window
+code_label = Label(left_frame, text="Enter code here:", anchor=W,pady=5)
+
 #code is the window in which the code is written
-code = Text(root, width=50, height = window_height/20+4)
-code.grid(row=0, rowspan=1, columnspan=2)
-
-#car object
-car = Car()
-
-#canvas is where the car will go
-canvas_frame= Frame(root, width = window_width/1.5, height = code.winfo_height()*525)
-canvas_frame.configure(borderwidth=1.5,background='black')
-canvas_frame.grid(row=0,column=2)
-canvas = Canvas(canvas_frame, width = window_width/1.5, height = code.winfo_height()*525)
-car.image = Image.open('car.png')
-car.image_tk = ImageTk.PhotoImage(car.image)
-#print car_image.format, car_image.size, car_image.mode
-car.car_object = canvas.create_image(30,250,image=car.image_tk)
-canvas.pack()
+code = Text(left_frame, width=50, height = 42)
 
 #run_button passes code into a run program method
-run_button = Button(root, text = "Run", command = lambda: generate_program(code.get(1.0,END)))
-run_button.grid(row=1,column=0)
+run_button = Button(left_frame, text = "Run Code", pady=5, padx=5, command = lambda: generate_program(code.get(1.0,END)))
 
 #clear_button clears the code in the text box
-clear_button = Button(root, text = "Clear code", command = lambda: clear())
-clear_button.grid(row=1,column=1)
+clear_button = Button(left_frame, text = "Clear Code", command = clear)
 
-#code to add widgets goes here
+#canvas is where the car will go
+canvas_frame= Frame(root, width = window_width/1.5, height = window_height-300,padx=2,pady=2)
+canvas_frame.configure(borderwidth=1.5,background='black')
+canvas = Canvas(canvas_frame, width = window_width/1.5, height = window_height-300)
+car.image = Image.open('images/car.png')
+car.image_tk = ImageTk.PhotoImage(car.image)
+car.car_object = canvas.create_image(30,250,image=car.image_tk)
+
+#label above the console
+console_label = Label(root, text = "Console", anchor=W,pady=5)
+
+#console to print to
+console = Text(root, width = int(window_width/1.5), height = 10, padx=2, pady=2)
+
+#add them to canvas
+left_frame.pack(side=LEFT)
+code_label.pack();
+code.pack()
+run_button.pack(side=LEFT)
+clear_button.pack(side=RIGHT)
+canvas_frame.pack()
+canvas.pack()
+console_label.pack()
+console.pack()
+
 root.mainloop()
