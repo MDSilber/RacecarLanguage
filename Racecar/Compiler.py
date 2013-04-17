eb9a2ed78e1b3004f05bb717368ab0ea092978fb
Sam <therunner05@gmail.com>
2013-04-17 18:16:10 -0400

    PEP8 compatibility and update on Compiler problem
    Compiler wasn't recognizing "unicode" strings but now it will with
    isinstance(translator, basestring)

--- Racecar/Compiler.py
+++ Racecar/Compiler.py
@@ -1,5 +1,6 @@
 from Parser import parseString
 
+
 def getPythonCode(code):
 
     #return "translate_car(5, CarDirection.FORWARDS)"
@@ -7,7 +8,7 @@ def getPythonCode(code):
     ast = parseString(code)
 
     if len(ast.errors) > 0:
-      return (None, ast.errors)
+        return (None, ast.errors)
 
     # then run the string through the semantic analyzer
     # ast = runSemanticAnalyzer(ast)
@@ -23,37 +24,38 @@ def generatePythonCode(ast):
 
     # potential AST values and their associated translation functions
     astTranslators = {
-      "ID" : idTranslator,
-      "assignment_command" : assignmentCommandTranslator,
-      "backward" : backwardTranslator,
-      "backwards" : backwardTranslator,
-      "declaration_command" : declarationCommandTranslator,
-      "define_command" : defineCommandTranslator,
-      "drive_command" : driveCommandTranslator,
-      "empty" : emptyTranslator,
-      "forward" : forwardTranslator,
-      "forwards" : forwardTranslator,
-      "function_command" : functionCommandTranslator,
-      "left" : leftTranslator,
-      "opt_parameters" : optParametersTranslator,
-      "plus_expression" : plusExpressionTranslator,
-      "print" : printTranslator,
-      "right" : rightTranslator,
-      "statement_block" : statementBlockTranslator,
-      "statements" : statementsTranslator,
-      "times_expression" : timesExpressionTranslator,
-      "turn_command" : turnCommandTranslator,
+        "ID": idTranslator,
+        "assignment_command": assignmentCommandTranslator,
+        "backward": backwardTranslator,
+        "backwards": backwardTranslator,
+        "declaration_command": declarationCommandTranslator,
+        "define_command": defineCommandTranslator,
+        "drive_command": driveCommandTranslator,
+        "empty": emptyTranslator,
+        "forward": forwardTranslator,
+        "forwards": forwardTranslator,
+        "function_command": functionCommandTranslator,
+        "left": leftTranslator,
+        "opt_parameters": optParametersTranslator,
+        "plus_expression": plusExpressionTranslator,
+        "print": printTranslator,
+        "right": rightTranslator,
+        "statement_block": statementBlockTranslator,
+        "statements": statementsTranslator,
+        "times_expression": timesExpressionTranslator,
+        "turn_command": turnCommandTranslator,
     }
 
     # "declare" pythonCode
     pythonCode = None
 
     # Fetch the appropriate translator function from astTranslators
-    # If there is no translator for ast.value then just let the translator be ast.value
+    # If there is no translator for ast.value then just let the
+    # translator be ast.value
     translator = astTranslators.get(ast.value, ast.value)
 
     # If the "translator" is just a string, then return that translator
-    if type(translator) == type("") or type(translator) == type(u''):
+    if isinstance(translator, basestring):
         pythonCode = ast.value
 
     # if the translator is a real function then invoke it
@@ -70,13 +72,16 @@ def indentLines(unindentedLines):
     pythonCode = "    " + "    ".join(splitCode)
     return pythonCode
 
+
 def emptyTranslator(ast):
     return ""
+
 
 def statementsTranslator(ast):
     pythonCode = generatePythonCode(ast.children[0])
     pythonCode += generatePythonCode(ast.children[1])
     return pythonCode
+
 
 def driveCommandTranslator(ast):
     # drive numSteps direction steps -->
@@ -87,13 +92,16 @@ def driveCommandTranslator(ast):
     pythonCode += ")\n"
     return pythonCode
 
+
 def forwardTranslator(ast):
     pythonCode = "CarDirection.FORWARDS"
     return pythonCode
 
+
 def backwardTranslator(ast):
     pythonCode = "CarDirection.BACKWARDS"
     return pythonCode
+
 
 def turnCommandTranslator(ast):
     pythonCode = "rotate_car("
@@ -101,13 +109,16 @@ def turnCommandTranslator(ast):
     pythonCode += ")\n"
     return pythonCode
 
+
 def leftTranslator(ast):
     pythonCode = "WheelDirection.LEFT"
     return pythonCode
 
+
 def rightTranslator(ast):
     pythonCode = "WheelDirection.RIGHT"
     return pythonCode
+
 
 def declarationCommandTranslator(ast):
     # id is a whatever -->
@@ -116,9 +127,11 @@ def declarationCommandTranslator(ast):
     pythonCode += " = None\n"
     return pythonCode
 
+
 def idTranslator(ast):
     pythonCode = ast.value
     return pythonCode
+
 
 def assignmentCommandTranslator(ast):
     pythonCode = generatePythonCode(ast.children[1])
@@ -127,18 +140,21 @@ def assignmentCommandTranslator(ast):
     pythonCode += "\n"
     return pythonCode
 
+
 def printTranslator(ast):
-      pythonCode = "print_to_console("
-      pythonCode += generatePythonCode(ast.children[1])
-      pythonCode += ")\n"
-      return pythonCode
+    pythonCode = "print_to_console("
+    pythonCode += generatePythonCode(ast.children[1])
+    pythonCode += ")\n"
+    return pythonCode
+
 
 def defineCommandTranslator(ast):
-      pythonCode = "def "
-      pythonCode += generatePythonCode(ast.children[1])
-      pythonCode += "():\n"
-      pythonCode += generatePythonCode(ast.children[4])
-      return pythonCode
+    pythonCode = "def "
+    pythonCode += generatePythonCode(ast.children[1])
+    pythonCode += "():\n"
+    pythonCode += generatePythonCode(ast.children[4])
+    return pythonCode
+
 
 def statementBlockTranslator(ast):
     prelimPythonCode = generatePythonCode(ast.children[1])
@@ -147,12 +163,14 @@ def statementBlockTranslator(ast):
 
     return pythonCode
 
+
 def functionCommandTranslator(ast):
     pythonCode = generatePythonCode(ast.children[0])
     pythonCode += "("
     pythonCode += generatePythonCode(ast.children[1])
     pythonCode += ")\n"
     return pythonCode
+
 
 def optParametersTranslator(ast):
     if len(ast.children) > 1:
@@ -163,6 +181,7 @@ def optParametersTranslator(ast):
     else:
         return ""
 
+
 def binaryOperatorTranslator(ast, op):
     pythonCode = "(("
     pythonCode += generatePythonCode(ast.children[0])
@@ -170,23 +189,24 @@ def binaryOperatorTranslator(ast, op):
     pythonCode += generatePythonCode(ast.children[2])
     pythonCode += "))"
     return pythonCode
-    
+
+
 def plusExpressionTranslator(ast):
     return binaryOperatorTranslator(ast, "+")
 
+
 def timesExpressionTranslator(ast):
     return binaryOperatorTranslator(ast, "*")
-            
+
 
 if __name__ == "__main__":
-	inputString = ''
-	while True:
-	
-		inputString = raw_input('enter expression > ')
+    inputString = ''
+    while True:
 
-		if inputString == 'exit':
-			break
+        inputString = raw_input('enter expression > ')
 
-		else:
-                        print getPythonCode(inputString)
+        if inputString == 'exit':
+            break
 
+        else:
+            print getPythonCode(inputString)
--- Racecar/Tree.py
+++ Racecar/Tree.py
@@ -1,30 +1,29 @@
 class Tree:
-  def __init__(self):
-    self.children = []
-    self.value = None
-    self.errors = []
+    def __init__(self):
+        self.children = []
+        self.value = None
+        self.errors = []
 
-  def printTree_old(self):
-    print "(value: ", self.value,
-    print ", children: [ ",
-    for tree in self.children:
-      if type(tree) == type(self):
-        tree.printTree()
-      else:
-        print tree,
-      print ", ",
-    print " ] ) ",
+    def printTree_old(self):
+        print "(value: ", self.value,
+        print ", children: [ ",
+        for tree in self.children:
+            if type(tree) == type(self):
+                tree.printTree()
+            else:
+                print tree,
+            print ", ",
+        print " ] ) ",
 
-
-  def printTree(self):
-    if self.value != "empty" and len(self.children) != 0:
-      print self.value, " -> ",
-      for child in self.children:
-        if type(child) == type(self):
-          print child.value, " ",
-        else:
-          print child, " ",
-      print
-      for child in self.children:
-        if type(child) == type(self):
-          child.printTree()
+    def printTree(self):
+        if self.value != "empty" and len(self.children) != 0:
+            print self.value, " -> ",
+            for child in self.children:
+                if type(child) == type(self):
+                    print child.value, " ",
+                else:
+                    print child, " ",
+            print
+            for child in self.children:
+                if type(child) == type(self):
+                    child.printTree()
