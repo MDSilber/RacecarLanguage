0bd26c906f3b90fc5d5dee848f5328a22c5155c5
Jeremy Spencer <jjs2195@columbia.edu>
2013-05-02 02:26:44 -0400

    fixed exec function

--- Racecar/RacecarGUI/racecar.py
+++ Racecar/RacecarGUI/racecar.py
@@ -578,7 +578,8 @@ def generate_program(code):
 
             #Toggle the buttons on the bottom and run program
             toggle_buttons(True)
-            exec(python_code, globals())
+            copy = globals().copy()
+            exec(python_code, copy)
             toggle_buttons(False)
 
             #If collision occurred
